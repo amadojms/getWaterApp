9c2d8e3fe50737dbacefeedd050e01e9416c74c6
AmadoEuro <amadojms@gmail.com>
2018-11-26 17:24:03 -0500

    Maquetacion del cascaron de la aplicacion

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,58 +30,57 @@
       app
     >
       <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
+    <!-- <v-footer
       :fixed="fixed"
       app
     >
       <span>&copy; 2017</span>
-    </v-footer>
+    </v-footer> -->
+    <v-card height="200px" flat>
+    <div class="headline text-xs-center pa-5">
+      Active: {{ bottomNav }}
+    </div>
+    <v-bottom-nav
+      :active.sync="bottomNav"
+      :value="true"
+      absolute
+      color="transparent"
+    >
+      <v-btn v-for="(tab, index) in tabs" :key="index"
+        color="teal"
+        flat
+        value="recent"
+      >
+        <span v-text="tab.title"></span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <!-- <v-btn
+        color="teal"
+        flat
+        value="favorites"
+      >
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn
+        color="teal"
+        flat
+        value="nearby"
+      >
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn> -->
+    </v-bottom-nav>
+  </v-card>
   </v-app>
 </template>
 
@@ -91,15 +90,17 @@
       return {
         clipped: false,
         drawer: true,
-        fixed: false,
         items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+          { icon: 'apps', title: 'Perfil', to: '/' },
+          { icon: 'bubble_chart', title: 'Ajustes', to: '/inspire' }
         ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        tabs:[
+          { icon: 'apps', title: 'Solicitar agua', to: '/' },
+          { icon: 'fingerprint', title: 'Perfil', to: '/' },
+          { icon: 'invert_colors', title: 'Programar entregas', to: '/' },
+        ],
+        title: 'Vuetify.js',
+        bottomNav: 'recent'
       }
     }
   }
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,18 +1,20 @@
 <template>
   <v-layout>
-    <v-flex text-xs-center>
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5">
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+    <v-flex>
+      <template>
+        <div>{{increment}} limones</div>
+        <button @click="$store.commit('increment')">Aumentar limones</button>
+      </template>
     </v-flex>
   </v-layout>
 </template>
+<script>
+export default {
+  computed: {
+    increment(){
+      return this.$store.state.counter
+    }
+  }
+}
+</script>
+
