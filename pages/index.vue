51910db09047c2bc3845c8bbe1f5dec0d25776f5
AmadoEuro <amadojms@gmail.com>
2018-11-29 18:44:29 -0500

    update getWater.vue, add img bottle

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,38 @@
 <template>
   <v-container grid-list-lg>
-    <v-layout column align-center>
-      <v-flex xs12 sm8 md6>
-        <div class="subheading">¿Que deseas hacer?</div>
+    <v-layout column>
+      <v-flex>
+        <div class="display-3 text-xs-center">¿Que deseas hacer?</div>
         <!-- <logo/> -->
       </v-flex>
-    </v-layout>
-    <v-layout wrap>
       <v-flex xs12 sm12 md12>
         <v-card color="blue-grey darken-2" class="white--text" to="users/consumption">
-          <v-card-title primary-title>
+          <v-card-text class="text-xs-center">
             <div>
               <div class="headline">Consumo de agua</div>
               <span>Resumen del consumo de agua por mes, semana o dia.</span>
             </div>
-          </v-card-title>
+          </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12>
-        <v-card color="cyan darken-2" class="white--text text-xs-center" to="users/getWater">
-          <v-card-title primary-title>
+      <v-flex xs12 sm12 md12>
+        <v-card color="cyan darken-2" class="white--text" to="users/getWater">
+          <v-card-text class="text-xs-center">
             <div>
               <div class="headline">Solicitar agua</div>
               <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
             </div>
-          </v-card-title>
+          </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex xs12>
-        <v-card color="blue-grey darken-2" class="white--text text-xs-center" to="users/messages">
-          <v-card-title primary-title>
+      <v-flex xs12 sm12 md12>
+        <v-card color="blue-grey darken-2" class="white--text" to="users/messages">
+          <v-card-text class="text-xs-center">
             <div>
               <div class="headline">Mensajes</div>
               <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
             </div>
-          </v-card-title>
+          </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
--- pages/users/getWater.vue
+++ pages/users/getWater.vue
@@ -1,24 +1,30 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <v-card color="blue-grey darken-2" class="white--text" to="users/consumption">
-          <v-card-title primary-title>
-            <div>
+  <main>
+    <v-container fluid fill-height>
+      <v-layout flex align-center justify-center>
+        <v-flex md9>
+          <v-card color="blue-grey darken-2" class="white--text">
+            <v-card-text class="text-xs-center">
               <div class="headline">Solicitar agua</div>
-              <span>Resumen del consumo de agua por mes, semana o dia.</span>
-            </div>
-          </v-card-title>
-        </v-card>
-    </v-flex>
-  </v-layout>
+              <div>
+                <v-img src="../../static/img/bottle-of-water.png"></v-img>
+              </div>
+              <v-btn color="error"><v-icon>more</v-icon></v-btn>
+              <v-btn color="success"></v-btn>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </main>
 </template>
 <script>
 export default {
   computed: {
-    increment(){
-      return this.$store.state.counter
+    increment() {
+      return this.$store.state.counter;
     }
   }
-}
+};
 </script>
 
